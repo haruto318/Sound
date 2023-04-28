7401a27c17b200e97a00cc804229e42057ab6e92
HarutoHamano <haruto.yhs318@gmail.com>
2023-04-28 23:49:39 +0900

    Add the function which can switch image at touch down and touch up

--- Sound/ViewController.swift
+++ Sound/ViewController.swift
@@ -11,17 +11,29 @@ import AVFoundation
 
 class ViewController: UIViewController {
     
+    @IBOutlet var drumButton: UIButton!
+    
     let drumSoundPlayer = try!AVAudioPlayer(data: NSDataAsset(name: "drumSound")!.data )
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
-    @IBAction func tapDrumButton(){
+    @IBAction func touchDownDrumButton(){
+        
+        drumButton.setImage(UIImage(named: "drumPlayingImage"), for: .normal)
+        
         drumSoundPlayer.currentTime = 0
         
         drumSoundPlayer.play()
+    }
+    
+    @IBAction func touchUpDrumButton(){
+        
+        drumButton.setImage(UIImage(named: "drumImage"), for: .normal)
+        
     }
 
 }
